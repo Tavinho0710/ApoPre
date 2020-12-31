b5341f51c70061b4a51cd3d419b2ea2b8b9119b4
gustavo-niehues@hotmail.com <$0710Gusta$>
2020-12-31 19:29:49 -0300

    Configuração de banco de dados inicial

--- db.py
+++ db.py
@@ -9,13 +9,10 @@ class Database:
 	def service(self):
 		pass
 
-	def __init__(self):
+	def __init__(self, database, user, password):
 		conn = pyodbc.connect(
-			'DSN=Sapiens;UID=sa;PWD=pds2772@', timeout=1)
+			'DSN={0};UID={1};PWD={2}'.format(database, user, password), timeout=1)
 		cursor = conn.cursor()
 		cursor.execute('Select * from usu_tetiqbag')
 		for row in cursor.fetchall():
 			print(row)
-
-
-Database()
--- main.py
+++ main.py
@@ -3,17 +3,21 @@ import time
 import configparser
 import logging
 from lcd import Lcd
-# from db import Database
+from db import Database
 
 
 class Apontamento:
 	def __init__(self):
 		logging.info('Inicializando')
-		logging.basicConfig(filename='apontamento.log', )
-		print('Inicializando')
+		logging.basicConfig(filename='apontamento.log', encoding='utf-8', level=logging.DEBUG)
 		self.lcd = Lcd()
+		self.cp = configparser.ConfigParser()
+		self.cp.read('config.ini')
+		database = self.cp.get('sistema', 'database')
+		user = self.cp.get('sistema', 'user')
+		pwd = self.cp.get('sistema', 'password')
+		self.db = Database()
 		time.sleep(5)
-		# self.db = Database()
 
 
 if __name__ == '__main__':
