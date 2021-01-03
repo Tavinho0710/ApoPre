d4057e6d67c38c84edbe58fb9f5fb9fa9d875697
gustavo-niehues@hotmail.com <$0710Gusta$>
2021-01-03 00:39:30 -0300

    Configuração de logging e tela

--- db.py
+++ db.py
@@ -11,12 +11,13 @@ class Database:
 		pass
 
 	def __init__(self, database, user, password, mode):
-		logging.basicConfig(filename='db.log', level=mode)
-		conn = pyodbc.connect(
-			'DSN={0};UID={1};PWD={2}'.format(database, user, password), timeout=1)
-		cursor = conn.cursor()
-		cursor.execute('Select * from usu_tetiqbag')
-		logging.info('Banco de dados iniciado')
-		for row in cursor.fetchall():
-			print(row)
-
+		try:
+			conn = pyodbc.connect(
+				'DSN={0};UID={1};PWD={2}'.format(database, user, password), timeout=1)
+			cursor = conn.cursor()
+			cursor.execute('Select * from usu_tetiqbag')
+			logging.info('Banco de dados iniciado')
+			for row in cursor.fetchall():
+				print(row)
+		except Exception as e:
+			logging.error('Falha na conexão com o banco de dados. ' + str(e))
--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class Apontamento:
 		self.cp.read('config.ini')
 		mode = int(self.cp.get('modo', 'modo'))
 		logging.basicConfig(filename='apontamento.log', level=mode)
+
 		logging.info('Inicializando')
 
 		database = self.cp.get('sistema', 'database')
@@ -21,15 +22,26 @@ class Apontamento:
 		pwd = self.cp.get('sistema', 'password')
 
 		logging.info('Incialização da base de dados')
+
 		self.db = Database(database, user, pwd, mode)
 		time.sleep(5)
+
+		logging.info('Carregando dados de apontamento')
+
+		self.codemp = self.cp.get('apontamento', 'codemp')
+		self.codfil = self.cp.get('apontamento', 'codfil')
+		self.codope = self.cp.get('apontamento', 'codope')
+		self.codori = self.cp.get('apontamento', 'codori')
+		self.numorp = self.cp.get('apontamento', 'numorp')
+		self.qtdprv = self.cp.get('apontamento', 'qtdprv')
+		self.qtdfrd = self.cp.get('apontamento', 'qtdfrd')
 
 		status = threading.Thread(target=self.status_t)
 		status.start()
 
 	def status_t(self):
 		while True:
-			self.lcd.write_line('Teste 1', 0, 0, 0)
+			self.lcd.write_line('OP: ' + self.numorp, 0, 0, 0)
 			self.lcd.write_line('Teste 2', 1, 0, 0)
 			self.lcd.write_line('Teste 3', 2, 0, 0)
 			self.lcd.write_line('Teste 4', 3, 0, 0)
