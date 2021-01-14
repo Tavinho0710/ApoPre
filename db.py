8dba46dc3a9799824879fc4c5d29213d750e4534
gustavo-niehues@hotmail.com <$0710Gusta$>
2021-01-14 08:33:43 -0300

    Banco de dados

--- db.py
+++ db.py
@@ -1,13 +1,20 @@
 import logging
 import sqlite3
+import time
 import threading
 import pyodbc
+from datetime import datetime
 
 
 class Database:
-	def insert_entry(self):
-		pass
-
+	def insert_entry(self, codemp, codfil, codope, codori, numorp, codbar, cont, cel):
+		datapo = datetime.now.strftime("%d/%m/%Y, %H:%M:%S")
+		query = 'select * from usu_tetiqbag where usu_codbar = {0}'.format(codbar)
+		if self.get_status():
+			rs = self.conexao_cursor.execute(query)
+			if rs:
+				print(rs)
+			
 	def get_status(self):
 		if self.conexao:
 			return True
@@ -15,11 +22,15 @@ class Database:
 			return False
 		
 	def service_t(self):
+		self.local = sqlite3.connect('sapiens_backup.db', check_same_thread=False)
+		self.local_cursor = self.local.cursor()
+		self.create_table()
 		self.start_db()
 		while True:
 			if self.conexao:
 				pass
 			else:
+				time.sleep(3)
 				self.start_db()
 				
 	def start_db(self):
@@ -30,15 +41,39 @@ class Database:
 			cursor.execute('Select * from usu_tetiqbag')
 			logging.info('Banco de dados iniciado')
 			self.conexao = conn
+			self.conexao_cursor = conn.cursor()
+			
 		except Exception as e:
 			logging.error('Falha na conexão com o banco de dados. ' + str(e))
 			self.conexao = None
+	
+	def create_table(self):
+		query = """
+					CREATE TABLE IF NOT EXISTS usu_tetiqbag (
+					usu_codemp INT,
+					usu_codfil INT,
+					usu_codope INT,
+					usu_codori TEXT,
+					usu_numorp INT,
+					usu_codbar TEXT,
+					usu_seqbar INT,
+					usu_datapo TEXT,
+					usu_celapo INT,
+					usu_sitapo INT
+					);"""
+		self.local_cursor.execute(query)
+		self.local.commit()
 	
 	def __init__(self, database, user, password):
 		self.database = database
 		self.user = user
 		self.password = password
 		self.conexao = None
+		
+		self.conexao_cursor = None
+		
+		self.local = None
+		self.local_cursor = None
 		
 		service = threading.Thread(target=self.service_t)
 		service.start()
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import threading
 import time
 import configparser
 import logging
+import sys
+from datetime import datetime
 from lcd import Lcd
 from db import Database
 
@@ -10,8 +12,12 @@ class Apontamento:
 	def __init__(self):
 		self.lcd = Lcd()
 		self.cp = configparser.ConfigParser()
-		
-		self.cp.read('config.ini')
+		try:
+			self.cp.read('config.ini')
+		except Exception as e:
+			logging.error('' + str(e) + ' ' + str((datetime.now().strftime('%d/%m/%Y %H:%M:%S'))))
+			logging.error('Erro na leitura/gravação do arquivo de config')
+			sys.exit()
 		mode = int(self.cp.get('modo', 'modo'))
 		logging.basicConfig(filename='apontamento.log',
 		                    level=mode,
@@ -31,13 +37,13 @@ class Apontamento:
 		
 		logging.info('Carregando dados de apontamento')
 		
-		self.codemp = self.cp.get('apontamento', 'codemp')
-		self.codfil = self.cp.get('apontamento', 'codfil')
-		self.codope = self.cp.get('apontamento', 'codope')
+		self.codemp = int(self.cp.get('apontamento', 'codemp'))
+		self.codfil = int(self.cp.get('apontamento', 'codfil'))
+		self.codope = int(self.cp.get('apontamento', 'codope'))
 		self.codori = self.cp.get('apontamento', 'codori')
-		self.numorp = self.cp.get('apontamento', 'numorp')
-		self.qtdprv = self.cp.get('apontamento', 'qtdprv')
-		self.qtdfrd = self.cp.get('apontamento', 'qtdfrd')
+		self.numorp = int(self.cp.get('apontamento', 'numorp'))
+		self.qtdprv = int(self.cp.get('apontamento', 'qtdprv'))
+		self.qtdfrd = int(self.cp.get('apontamento', 'qtdfrd'))
 		
 		status = threading.Thread(target=self.status_t)
 		status.start()
@@ -45,7 +51,28 @@ class Apontamento:
 		logging.info('Início da leitura do código de barras')
 		
 		while True:
-			pass
+			codbar: str = input()
+			logging.debug('Cod. Barras lido: {0}'.format(codbar))
+			if len(codbar) == 10:
+				op, contcel = codbar.split('-')
+				cont, cel = int(contcel[0:3]), int(contcel[-1])
+				op = int(op)
+				
+				if self.numorp == op:
+					result = self.db.insert_entry(self.codemp,
+					                              self.codfil,
+					                              self.codope,
+					                              self.codori,
+					                              self.numorp,
+					                              codbar,
+					                              cont,
+					                              cel)
+					if result == 0:
+						self.lcd.write_line('Já apontado', 0, 1, 2)
+					if result == 1:
+						pass
+			elif len(codbar) == 24:
+				nova_op = int(codbar[2:11])
 	
 	def status_t(self):
 		while True:
@@ -57,7 +84,7 @@ class Apontamento:
 			                    + 'Ult: 0', 0, 0, 0)
 			self.lcd.write_line('Teste 2', 1, 0, 0)
 			self.lcd.write_line('Teste 3', 2, 0, 0)
-			self.lcd.write_line('C:'+('S' if self.db.get_status() else 'N'), 3, 0, 0)
+			self.lcd.write_line('C:' + ('S' if self.db.get_status() else 'N'), 3, 0, 0)
 			time.sleep(3)
 
 
